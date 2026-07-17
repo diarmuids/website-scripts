6d9acb98f060860fd67c83874dd3a1bf38205990
diarmuids <diarmuids@gmail.com>
2026-07-17 12:52:58 +0100

    Auto-publish website scripts

---
+++ sites/longmile.js
@@ -0,0 +1,17 @@
+// $('#Existing-Patient').on('change', function () {
+//   if ($(this).val() === 'No') {
+//     $('.form_yes-existing').hide();
+//     $('.form_no-existing').show();
+//   } else if ($(this).val() === 'Yes') {
+//     $('.form_yes-existing').show();
+//     $('.form_no-existing').hide();
+//   }
+// });
+
+// $('a[href="#message-us"]').on('click', function (event) {
+//   $('.tabs_component').find('[data-w-tab="Message"]').trigger('click');
+// });
+
+// $('a[href="#book-appointment"]').on('click', function (event) {
+//   $('.tabs_component').find('[data-w-tab="Appointment"]').trigger('click');
+// });
