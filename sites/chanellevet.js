f27e6d6c81d1a1022625f8a53151ab2d8a34e16b
diarmuids <diarmuids@gmail.com>
2026-07-17 12:32:52 +0100

    Auto-publish website scripts

---
+++ sites/chanellevet.js
@@ -0,0 +1,19 @@
+// $('.marcat-list').append($('.btn-marketing-reset'));
+
+// $(document).ready(function () {
+//   $('#videoDownload').on('click', function () {
+//     var fileUrl = 'https://example.com/path-to-your-file.zip'; // Replace with your file URL
+
+//     // Create a temporary anchor element
+//     var a = document.createElement('a');
+//     a.href = fileUrl;
+//     a.target = '_blank'; // This is optional, in case you want to open it in a new tab
+//     a.download =
+//       ''; // Leave this empty to use the default file name or specify a custom name
+
+//     // Append the anchor to the document, trigger the click, and then remove it
+//     document.body.appendChild(a);
+//     a.click();
+//     document.body.removeChild(a);
+//   });
+// });
