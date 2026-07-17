b3fb375f05574426472b0e98da7a64f65f156181
diarmuids <diarmuids@gmail.com>
2026-07-17 12:41:23 +0100

    Auto-publish website scripts

---
+++ sites/mungretmc copy.js
@@ -0,0 +1,28 @@
+// alert("TEST")
+
+// // HIDE NAV ON FILLOUT FORM VALIDATION
+// var position1 = $(window).scrollTop(); // Initial scroll position
+// $(window).scroll(function () {
+//   var scroll1 = $(window).scrollTop();
+//   var div = $('.form_fillout-embed');
+//   var divTop = div.offset().top;
+//   var divBottom = div.offset().top + div.outerHeight();
+//   // IF SCROLLED ABOVE FORM
+//   if ($(window).scrollTop() < divTop - 120) {
+//     $(".nav_component").removeClass("nav-hidden");
+//   }
+//   // IF FORM BUTTON IN VIEW
+//   else if (divBottom - 125 <= $(window).scrollTop() + $(window).height()) {
+//     $(".nav_component").addClass("nav-hidden");
+//   }
+//   // IF SCROLLED UP WHEN FORM BUTTON IN VIEW
+//   else if (position1 - scroll1 > 0 && divBottom - 120 <= $(window).scrollTop() + $(window)
+//     .height()) {
+//     $(".nav_component").addClass("nav-hidden");
+//   }
+//   // IF SCROLLED DOWNWARDS
+//   else if (position1 - scroll1 < -20) {
+//     $(".nav_component").removeClass("nav-hidden");
+//   }
+//   position1 = scroll1;
+// });
