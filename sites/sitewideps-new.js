5bd30491d0db18d5a9442e3ef6db28d7a6a438c4
diarmuids <diarmuids@gmail.com>
2026-09-19 15:17:41 +0100

    Publish sitewideps-new.js starter script

---
+++ sites/sitewideps-new.js
@@ -0,0 +1,6 @@
+// Last updated: 2026-09-19 15:17:41
+
+// Sitewide PS: new-build site script (sites/sitewideps-new.js).
+// Loaded by the new Webflow build's footer loader: dev.wsitefiles.com/sites/sitewideps-new.js,
+// falling back to wsitefiles.com/sites/sitewideps-new.js. The live site keeps its own script.
+(() => {})();
