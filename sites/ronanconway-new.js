ce497788080756b22825e11606b68864b8f00c46
diarmuids <diarmuids@gmail.com>
2026-09-19 15:17:37 +0100

    Publish ronanconway-new.js starter script

---
+++ sites/ronanconway-new.js
@@ -0,0 +1,6 @@
+// Last updated: 2026-09-19 15:17:37
+
+// Ronan Conway: new-build site script (sites/ronanconway-new.js).
+// Loaded by the new Webflow build's footer loader: dev.wsitefiles.com/sites/ronanconway-new.js,
+// falling back to wsitefiles.com/sites/ronanconway-new.js. The live site keeps its own script.
+(() => {})();
